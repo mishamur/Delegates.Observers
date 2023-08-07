98acb6a640021934f37dd834d693b4bfa34868a2
Mikhail Kotov <misha.kotov.01@list.ru>
2023-08-07 17:03:03 +0400

    Добавьте файлы проекта.

---
+++ ObservableStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegates.Observers
+{
+    public delegate void StackHandler(object obj);
+    public class StackOperationsLogger
+	{
+        public StringBuilder Log = new StringBuilder();
+
+        public void HandleEvent(object eventData)
+        {
+            Log.Append(eventData);
+        }
+
+		public void SubscribeOn<T>(ObservableStack<T> stack)
+		{
+			stack.Add(HandleEvent);
+		}
+
+		public string GetLog()
+		{
+			return Log.ToString();
+		}
+	}
+
+	public class ObservableStack<T>
+	{
+        public event StackHandler NotifyEvent;
+
+        private List<T> data = new List<T>();
+        public void Add(Action<object> action)
+		{
+			NotifyEvent += action.Invoke;
+		}
+
+		public void Notify(object eventData)
+		{
+			NotifyEvent?.Invoke(eventData);
+		}
+
+		public void Remove(Action<object> action)
+		{
+            NotifyEvent -= action.Invoke;
+        }
+
+		public void Push(T obj)
+		{
+			data.Add(obj);
+			Notify(new StackEventData<T> { IsPushed = true, Value = obj });
+		}
+
+		public T Pop()
+		{
+			if (data.Count == 0)
+				throw new InvalidOperationException();
+			var result = data.Last();
+			Notify(new StackEventData<T> { IsPushed = false, Value = result });
+			return result;
+
+		}
+	}
+}
---
+++ ObservableStack_should.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Observers
+{
+	[TestFixture]
+	public class ObservableStack_Tests
+	{
+		[Test]
+		public void Log_ShouldBeEmpty_AfterCreation()
+		{
+			var stack = new ObservableStack<int>();
+			var helper = new StackOperationsLogger();
+			helper.SubscribeOn(stack);
+			Assert.AreEqual("", helper.GetLog());
+		}
+
+		[Test]
+		public void Log_ShouldContainAllOperations()
+		{
+			var stack = new ObservableStack<int>();
+			var helper = new StackOperationsLogger();
+			helper.SubscribeOn(stack);
+			stack.Push(1);
+			stack.Push(2);
+			stack.Pop();
+			stack.Push(10);
+			Assert.AreEqual("+1+2-2+10", helper.GetLog());
+		}
+
+        [Test]
+        public void Log_ShouldContainAllOperations2()
+        {
+            var stack = new ObservableStack<string>();
+            var helper = new StackOperationsLogger();
+            helper.SubscribeOn(stack);
+            stack.Push("+4");
+            stack.Push("2");
+            stack.Pop();
+            stack.Push("12");
+            Assert.AreEqual("++4+2-2+12", helper.GetLog());
+        }
+    }
+}
